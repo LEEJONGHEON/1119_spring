fdb40de85cee6aa6c232a20ad39cb5e2acc17eea
LEEJONGHEON <tlqkfwjrekdgl@gmail.com>
2022-11-27 17:18:13 +0900

    update

--- src/main/java/com/example/demo/todo/api/TodoApiController.java
+++ src/main/java/com/example/demo/todo/api/TodoApiController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api/todos")
 @RequiredArgsConstructor
+@CrossOrigin(origins="*", allowedHeaders = "*")
 public class TodoApiController {
 
     private final TodoService service;
